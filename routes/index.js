d52b1d732696c03007f3aa9a78b4df577ca2e80e
thanh nguyen <thanh.uou@gmail.com>
2021-05-24 00:22:42 +0900

    add try catch for google_login api

--- routes/index.js
+++ routes/index.js
@@ -28,19 +28,24 @@ router.post('/google_login', async (req, res, next) => {
 });
 
 async function verify(token) {
-  const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: process.env.GOOGLE_CLIENT_ID, 
-      // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
-  const payload = ticket.getPayload();
-  const userid = payload['sub'];
+  try{
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID, 
+        // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
 
-  return userid;
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
+    return userid;
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+  }catch(ex){
+    console.log(ex);
+  }
+  return null;
 }
 
 module.exports = router;
